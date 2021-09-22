7fe1e53d473cdd691ebf436d70d5c35c927fef23
EleniTerzidou <36399110+EleniTerzidou@users.noreply.github.com>
2021-09-22 18:22:31 +0200

    Update FieldCheckboxGroup.js

--- src/components/FieldCheckboxGroup/FieldCheckboxGroup.js
+++ src/components/FieldCheckboxGroup/FieldCheckboxGroup.js
@@ -1,152 +1,77 @@
+/*
+ * Renders a group of checkboxes that can be used to select
+ * multiple values from a set of options.
+ *
+ * The corresponding component when rendering the selected
+ * values is PropertyGroup.
+ *
+ */
+
 import React from 'react';
-import { Form as FinalForm, FormSpy } from 'react-final-form';
-import arrayMutators from 'final-form-arrays';
-import { Button } from '../../components';
-import FieldCheckboxGroup from './FieldCheckboxGroup';
-import { requiredFieldArrayCheckbox } from '../../util/validators';
+import { arrayOf, bool, node, shape, string } from 'prop-types';
+import classNames from 'classnames';
+import { FieldArray } from 'react-final-form-arrays';
+import { FieldCheckbox, ValidationError } from '../../components';
 
-const formName = 'Styleguide.FieldCheckboxGroup';
-const formNameRequired = 'Styleguide.FieldCheckboxGroupRequired';
+import css from './FieldCheckboxGroup.module.css';
 
-const label = <h3>Amenities</h3>;
+const FieldCheckboxRenderer = props => {
+  const { className, rootClassName, label, twoColumns, id, fields, options, meta } = props;
 
-const commonProps = {
-  label: label,
-  options: [
-    {
-      key: 'towels',
-      label: 'Testttt',
-    },
-    {
-      key: 'bathroom',
-      label: 'Bathroom',
-    },
-    {
-      key: 'swimming_pool',
-      label: 'Swimming pool',
-    },
-    {
-      key: 'own_drinks',
-      label: 'Own drinks allowed',
-    },
-    {
-      key: 'jacuzzi',
-      label: 'Jacuzzi',
-    },
-    {
-      key: 'audiovisual_entertainment',
-      label: 'Audiovisual entertainment',
-    },
-    {
-      key: 'barbeque',
-      label: 'Barbeque',
-    },
-    {
-      key: 'own_food_allowed',
-      label: 'Own food allowed',
-    },
-  ],
-  twoColumns: true,
+  const classes = classNames(rootClassName || css.root, className);
+  const listClasses = twoColumns ? classNames(css.list, css.twoColumns) : css.list;
+
+  
+  return (
+    <fieldset className={classes}>
+      {label ? <legend>{label}</legend> : null}
+      <ul className={listClasses}>
+        {options.map((option, index) => {
+          const fieldId = `${id}.${option.key}`;
+          return (
+            <li key={fieldId} className={css.item}>
+              <FieldCheckbox
+                id={fieldId}
+                name={fields.name}
+                label={option.label}
+                value={option.key}
+              />
+            </li>
+          );
+        })}
+      </ul>
+      <ValidationError fieldMeta={{ ...meta }} />
+    </fieldset>
+  );
 };
 
-const optionalProps = {
-  name: 'amenities-optional',
-  id: 'amenities-optional',
-  ...commonProps,
+FieldCheckboxRenderer.defaultProps = {
+  rootClassName: null,
+  className: null,
+  label: null,
+  twoColumns: false,
 };
 
-const requiredProps = {
-  name: 'amenities-required',
-  id: `${formNameRequired}.amenities-required`,
-  ...commonProps,
-  validate: requiredFieldArrayCheckbox('this is required'),
-};
-const tosProps = {
-  name: 'terms-of-service',
-  id: `${formNameRequired}.tos-accepted`,
-  options: [
-    {
-      key: 'tos',
-      label: 'Terms of Service',
-    },
-  ],
-  validate: requiredFieldArrayCheckbox('You need to accept Terms of Service'),
+FieldCheckboxRenderer.propTypes = {
+  rootClassName: string,
+  className: string,
+  id: string.isRequired,
+  label: node,
+  options: arrayOf(
+    shape({
+      key: string.isRequired,
+      label: node.isRequired,
+    })
+  ).isRequired,
+  twoColumns: bool,
 };
 
-const formComponent = country => props => (
-  <FinalForm
-    {...props}
-    mutators={{ ...arrayMutators }}
-    render={fieldRenderProps => {
-      const { handleSubmit, invalid, submitting, componentProps, onChange } = fieldRenderProps;
+const FieldCheckboxGroup = props => <FieldArray component={FieldCheckboxRenderer} {...props} />;
 
-      const submitDisabled = invalid || submitting;
-
-      return (
-        <form
-          onSubmit={e => {
-            e.preventDefault();
-            handleSubmit(e);
-          }}
-        >
-          <FormSpy onChange={onChange} subscription={{ values: true, dirty: true }} />
-          <FieldCheckboxGroup {...componentProps} />
-
-          <Button style={{ marginTop: 24 }} type="submit" disabled={submitDisabled}>
-            Submit
-          </Button>
-        </form>
-      );
-    }}
-  />
-);
-
-export const Optional = {
-  component: formComponent(formName),
-  props: {
-    onChange: formState => {
-      if (formState.dirty) {
-        console.log('form values changed to:', formState.values);
-      }
-    },
-    onSubmit: values => {
-      console.log('Submit values: ', values);
-    },
-    initialValues: { [optionalProps.name]: ['jacuzzi', 'towels'] },
-    componentProps: optionalProps,
-  },
-  group: 'inputs',
+// Name and component are required fields for FieldArray.
+// Component-prop we define in this file, name needs to be passed in
+FieldCheckboxGroup.propTypes = {
+  name: string.isRequired,
 };
 
-export const Required = {
-  component: formComponent(formNameRequired),
-  props: {
-    onChange: formState => {
-      if (formState.dirty) {
-        console.log('form values changed to:', formState.values);
-      }
-    },
-    onSubmit: values => {
-      console.log('Submit values: ', values);
-    },
-    componentProps: requiredProps,
-  },
-  group: 'inputs',
-};
-
-export const ToSAccepted = {
-  component: formComponent(formNameRequired),
-  props: {
-    onChange: formState => {
-      if (formState.dirty) {
-        console.log('form values changed to:', formState.values);
-      }
-    },
-    onSubmit: values => {
-      console.log('Submit values: ', values);
-    },
-    componentProps: tosProps,
-  },
-  group: 'inputs',
-};
 export default FieldCheckboxGroup;
